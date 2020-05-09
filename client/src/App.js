3469aa9e58841f2f2ca7925da0cd99a35b4152d0
dtkachov <tkachov@localhost.localdomain>
2020-05-09 22:04:09 +0300

    Add Client react

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./commponets/header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
