bab4d8d888736901c2fe53754332c76eed845495
PaulMartyn <pashamartynov@hotmail.com>
2020-05-12 17:19:46 +0300

    new items

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import Header from "./commponets/header/Header";
 import SubmitBtn from "./commponets/buttons/SubmitBtn";
 import MainContainer from "./commponets/mainContainer/MainContainer";
+import MainCheckCategory from "./commponets/mainCheckCategory/MainCheckCategory";
 import Footer from "./commponets/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MainContainer />
-      <SubmitBtn />
-      <Footer />
+      {/* <MainContainer /> */}
+      <MainCheckCategory />
+      {/* <SubmitBtn /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
